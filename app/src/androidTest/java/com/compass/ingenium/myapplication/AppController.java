2db04fbd430b6ebfc62d9bf24c8f64f137db367d
Kaxell <akay_ertuqrul@yahoo.com>
2015-04-23 01:12:13 +0300

    register and login page

--- app/src/androidTest/java/com/compass/ingenium/myapplication/AppController.java
+++ app/src/androidTest/java/com/compass/ingenium/myapplication/AppController.java
@@ -8,12 +8,12 @@ package com.compass.ingenium.myapplication;
 
     //    package info.androidhive.loginandregistration.app;
 
-        import android.app.Application;
-        import android.text.TextUtils;
+import android.app.Application;
+import android.text.TextUtils;
 
-        import com.android.volley.Request;
-        import com.android.volley.RequestQueue;
-        import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class AppController extends Application {
 
