cca7e4b255191d88f1fce04eeb0d6a11015b1214
Kaxell <akay_ertuqrul@yahoo.com>
2015-04-23 00:59:04 +0300

    register and login page

---
+++ app/src/androidTest/java/com/compass/ingenium/myapplication/AppConfig.java
@@ -0,0 +1,17 @@
+package com.compass.ingenium.myapplication;
+
+/**
+ * By Ertuğrul Akay
+ * Don't touch this.
+ *
+ */
+  // package info.androidhive.loginandregistration.app;
+
+    public class AppConfig {
+        // Server user login url
+        public static String URL_LOGIN = "http://139.179.201.87/android_login_api/";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://139.179.201.87/android_login_api/";
+    }
+
