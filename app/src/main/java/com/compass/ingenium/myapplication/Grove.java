8fb83dd64d0e7098bdc3d89af620eca107056ba3
Merterm <mertincek@hotmail.com>
2015-04-22 21:40:01 +0300

    Added Model classes

--- app/src/main/java/com/compass/ingenium/myapplication/Grove.java
+++ app/src/main/java/com/compass/ingenium/myapplication/Grove.java
@@ -1,40 +1,37 @@
 package com.compass.ingenium.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.util.ArrayList;
 
-
-public class Grove extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grove);
-        User user = new User("Merterm", "12345", "mertincek@hotmail.com");
+public class Grove{
+    
+    //properties
+    protected ArrayList<Tree> trees;
+    
+    //constructors
+    public Grove()
+    {
+        trees = new ArrayList<Tree>();
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    
+    public Grove( ArrayList<Tree> trees)
+    {
+        this.trees = trees;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    
+    //methods
+    
+    public Tree getTree( int i)
+    {
+        return trees.get(i);
+    }
+    
+    public void addTree( Tree tree)
+    {
+        trees.add( tree);
+    }
+    
+    public void removeTree( int location)
+    {
+        trees.remove( location);
     }
 }
