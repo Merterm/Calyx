40ccd6ae83def6ad36e8928b4677f34c44a00e43
Merterm <mertincek@hotmail.com>
2015-04-23 16:03:31 +0300

    Login problems

--- app/src/main/java/com/compass/ingenium/myapplication/MainActivity.java
+++ app/src/main/java/com/compass/ingenium/myapplication/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import helper.SessionManager;
+
 public class MainActivity extends Activity {
 
     private TextView txtName;
