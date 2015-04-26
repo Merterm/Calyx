73f81198d6b81122bcdf668bb8258a7a2de97f9a
EkinDoesntSleep <ekin.uyumaz@ug.bilkent.edu.tr>
2015-04-26 20:37:26 +0300

    couldnt add the drawer to the menus but hopefully it will work when we can

--- app/src/main/java/com/compass/ingenium/myapplication/DrawerActivity.java
+++ app/src/main/java/com/compass/ingenium/myapplication/DrawerActivity.java
@@ -16,7 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.TextView; 
 
 
 public class DrawerActivity extends ActionBarActivity
